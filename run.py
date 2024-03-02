c4e86f7b3f7566975816b142acc019431f8031a3
AngoloTecnico <161149754+AngoloTecnico@users.noreply.github.com>
2024-03-02 16:32:47 +0100

    Add files via upload

---
+++ run.py
@@ -0,0 +1,22 @@
+import os
+import subprocess
+import glob
+
+GPU_ID = "1"
+MODEL_DIR = "./esrgan/models"
+INPUT_DIR = "./input/*.*"
+OUT_DIR = "./output"
+MODEL = "realesrgan-x4plus"
+CMD = "./esrgan/realesrgan-ncnn-vulkan.exe"
+
+
+def main():
+    files = glob.glob(INPUT_DIR)
+    for a in files:
+        fname = os.path.basename(a)
+        print("process: "+fname)
+        subprocess.run([CMD,"-i",a, "-o", os.path.join(OUT_DIR, fname), "-n", MODEL, "-g ", GPU_ID])
+ 
+
+if __name__ == "__main__":
+    main()
